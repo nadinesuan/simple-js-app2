ec8c835649346eade1b5c4e99b868e13375b604b
nadinesuan <119970805+nadinesuan@users.noreply.github.com>
2023-01-23 20:07:39 -0800

    variable
    updated variable to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-var favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: '7', types: ['grass', 'poison'] },
+    { name: 'Charmander', height: '6', types: 'fire'},
+    { name: 'Squirtle', height: '5', types: 'water'},
+    { name: 'Wigglytuff', height: '1', types: 'fairy'}
+];
