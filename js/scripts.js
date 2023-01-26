d0b60b5fb3852cc87540cd7e2fdbbdb654b17cbd
nadinesuan <119970805+nadinesuan@users.noreply.github.com>
2023-01-25 18:01:17 -0800

    array
    added for loops and conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: '7', types: ['grass', 'poison'] },
-    { name: 'Charmander', height: '6', types: 'fire'},
-    { name: 'Squirtle', height: '5', types: 'water'},
-    { name: 'Wigglytuff', height: '1', types: 'fairy'}
+    { name: "Bulbasaur", height: 0.7, types: ['grass', 'poison'] },
+    { name: "Charizard", height: 1.7, types: 'fire'},
+    { name: "Squirtle", height: 0.5, types: 'water'},
+    { name: "Wigglytuff", height: 1, types: 'fairy'}
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
+if (pokemonList[i].height >= 1.0) {
+    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - Wow, that's a big pokemon!");
+    }
+}
