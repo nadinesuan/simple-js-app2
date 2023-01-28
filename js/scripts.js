7f62054b3d7b313adde07a4541d008f60670aa97
nadinesuan <119970805+nadinesuan@users.noreply.github.com>
2023-01-28 13:51:40 -0800

    scripts.js
    replacing for loop with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,22 @@ let pokemonList = [
     { name: "Wigglytuff", height: 1, types: 'fairy'}
 ];
 
+/* before the forEach loop
 for (let i=0; i < pokemonList.length; i++) {
     document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
 if (pokemonList[i].height >= 1.0) {
     document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - Wow, that's a big pokemon!");
     }
 }
+*/
+
+//replacing for loop with forEach loop
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height >= 1.0) {
+        document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that's a big pokemon! + "<br>");
+    } else if (pokemon.height >= 0.7 && pokemon.height < 1.0) {
+        document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that's a medium pokemon! + "<br>");
+    } else { 
+        document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that's a small pokemon! + "<br>");
+    }
+});
